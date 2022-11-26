d60d2ad9bebe21d263682e7f9e239de7a470f4b0
julio <juliojm1310@gmail.com>
2022-11-26 15:07:31 +0300

    Add values to some options

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Diagnostic(models.Model):
+    created = models.DateTimeField(verbose_name='fecha del diagnostico', auto_now_add=True)
+    gr = models.BigIntegerField(verbose_name='gestion de riesgos', default=0)
+    ga = models.BigIntegerField(verbose_name='gestion de activos', default=0)
+    gia = models.BigIntegerField(verbose_name='gestion de identidad y acceso', default=0)
+    gav = models.BigIntegerField(verbose_name='gestion de amenazas y vulnerabilidades', default=0)
+    cea = models.BigIntegerField(verbose_name='conciencia del estado actual', default=0)
+    iic = models.BigIntegerField(verbose_name='intercambio de informacion y comunicaciones', default=0)
+    ri = models.BigIntegerField(verbose_name='respuesta a incidentes', default=0)
+    gde = models.BigIntegerField(verbose_name='gestion de dependencias externas', default=0)
+    cp = models.BigIntegerField(verbose_name='capacitacion del personal', default=0)
+    gpc = models.BigIntegerField(verbose_name='gestion de programa de ciberseguridad', default=0)
+    total = models.BigIntegerField(verbose_name='puntaje total', default=0)
+
+    def __str__(self):
+        return f'Diagnóstico {self.created}'
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.views.generic import RedirectView, TemplateView
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from .models import Diagnostic
 
 
 class IndexView(TemplateView):
@@ -39,5 +40,29 @@ def logout(request):
 @login_required
 def diagnostic(request):
     if request.method == 'POST':
-        pass
+        print(request.POST)
+        gr = 0
+        ga = 30
+        gia = 0
+        gav = 0
+        cea = 40
+        iic = 0
+        ri = 0
+        gde = 50
+        cp = 10
+        gpc = 0
+        total = gr + ga + gia + gav + cea + iic + ri + gde + cp + gpc
+        Diagnostic.objects.create(
+            gr=gr,
+            ga=ga,
+            gia=gia,
+            gav=gav,
+            cea=cea,
+            iic=iic,
+            ri=ri,
+            gde=gde,
+            cp=cp,
+            gpc=gpc,
+            total=total,
+        )
     return render(request, 'mainapp/diagnostic.html')
