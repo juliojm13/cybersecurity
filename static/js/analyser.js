62690a08832b988e1eb3b741a355dcbbbe2669c6
julio <juliojm1310@gmail.com>
2022-11-27 13:43:01 +0300

    Add template fro bar chart

---
+++ static/js/analyser.js
@@ -0,0 +1,9 @@
+ var canvasElement = document.getElementById("barChart")
+    var = config = {
+        type: "bar",
+        data: {
+        labels: [],
+        datasets: [],
+        },
+    }
+    var barChart = new Chart(canvasElement,config)
