97faf360942a53e4fad5c179566598348bbc6469
julio <juliojm1310@gmail.com>
2022-11-27 13:57:48 +0300

    Add bar chart logic

--- static/js/analyser.js
+++
@@ -1,9 +0,0 @@
- var canvasElement = document.getElementById("barChart")
-    var = config = {
-        type: "bar",
-        data: {
-        labels: [],
-        datasets: [],
-        },
-    }
-    var barChart = new Chart(canvasElement,config)
