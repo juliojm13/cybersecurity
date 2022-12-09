06e326ee51e89c39d9bff82ab2fb1e718a5c291e
julio <juliojm1310@gmail.com>
2022-12-09 22:17:46 +0300

    Add import,export to DB

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Diagnostic, Organisation
+from import_export.admin import ImportExportMixin
 
-admin.site.register(Diagnostic)
-admin.site.register(Organisation)
+
+class DiagnosticAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['created', 'gr', 'ga', 'gia', 'gav', 'cea', 'iic',
+                    'ri', 'gde', 'cp', 'gpc', 'total', 'id_organization']
+
+
+class OrganisationAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['org_name']
+
+
+admin.site.register(Diagnostic, DiagnosticAdmin)
+admin.site.register(Organisation, OrganisationAdmin)
